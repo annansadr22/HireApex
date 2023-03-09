9686175c44d3ad9cc0774cf442cf2a5bb7451d7e
annansadr22 <annansadr.ca@gmail.com>
2023-03-08 23:14:09 -0500

    hosting

--- HireApex/HireApex/urls.py
+++ HireApex/HireApex/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
-from django.views.static import serve
-from django.conf.urls import url
+#from django.views.static import serve
+#from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +28,6 @@ urlpatterns = [
     #path('contact-us/', TemplateView.as_view(template_name='extra/contactus.html'),name='contact_us'),
     path('accounts/', include('allauth.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    #url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
